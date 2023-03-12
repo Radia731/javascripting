36f6f7915d37337d2ca360f28524b431f9af13a9
Radia Ahmed <radiajjr17@gmail.com>
2023-03-11 20:51:44 -0500

    Iterating Over an Array With while

--- packing-list.js
+++ packing-list.js
@@ -1,4 +1,8 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
-  console.log(packingList[3]);
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
